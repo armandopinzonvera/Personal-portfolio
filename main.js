473f87cf90dbb8b4eb9c69981cd375f5c3f023e3
Armando Pinzon V <54361215+armandopinzonvera@users.noreply.github.com>
2019-11-29 10:06:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,24 +2,24 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var barra = $(window).scrollTop();
-        var posicion2 = barra * 0.9;
-        var posicion3 = barra * 0.7;
-        var posicion4 = barra * 0.5;
-        var posicion5 = barra * 0.4;
+        var posicion2 = barra * 0.2;
+        var posicion3 = barra * 0.4;
+        var posicion4 = barra * 0.6;
+        var posicion5 = barra * 0.8;
 
 
 
         $('#dos').css({
-            'background-position':'0 -' + posicion2 + 'px'
+            'background-position':'0 +' + posicion2 + 'px'
         });
         $('#tres').css({
-            'background-position':'0 -' + posicion3 + 'px'
+            'background-position':'0 +' + posicion3 + 'px'
         });
         $('#four').css({
-            'background-position':'0 -' + posicion4 + 'px'
+            'background-position':'0 +' + posicion4 + 'px'
         });
         $('#five').css({
-            'background-position':'0 -' + posicion5 + 'px'
+            'background-position':'0 +' + posicion5 + 'px'
         });       
 
     });
